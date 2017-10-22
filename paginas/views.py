41a6cfb2f946ba827400fe2817e0026f68cbd546
rewth Nunes <rewthnunes@gmail.com>
2017-10-22 19:37:06 -0200

    Inclusão CSS, arquivos requirements e Makefile

--- paginas/views.py
+++ paginas/views.py
@@ -1,12 +1,15 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+from .models import DadosPessoais
 from django.shortcuts import render
 
 # Create your views here.
 
 def pagina_exibir(request):
-	return render(request, 'paginas/pagina_exibir.html', {})
+
+	pessoa = DadosPessoais.objects.all()
+	context = {'pessoa': pessoa}
+	return render(request, 'paginas/pagina_exibir.html', context)
 
 
 
